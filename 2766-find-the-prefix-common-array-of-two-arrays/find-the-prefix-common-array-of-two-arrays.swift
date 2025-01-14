8f0a98e269980663017fb0b51657e61adb93cfa1
Aman gupta <aman19ish@gmail.com>
2025-01-14 13:12:42 +0530

    Time: 303 ms (7.69%) | Memory: 20.2 MB (7.69%) - LeetSync

---
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func findThePrefixCommonArray(_ A: [Int], _ B: [Int]) -> [Int] {
+        var ans = Set<Int>()
+        var result: [Int] = []
+        for i in 0..<A.count {
+            ans.insert(A[i])
+            ans.insert(B[i])
+            print(ans)
+            result.append( (i+1)*2 - ans.count)
+        }
+        return result
+    }
+}
